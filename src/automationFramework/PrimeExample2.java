6d07f9117c5194e3588a819787771b958503ae22
Pavan <Pavan@DESKTOP-RJ14E94>
2018-08-21 15:09:24 +0530

    Added a simple statement

--- src/automationFramework/PrimeExample2.java
+++ src/automationFramework/PrimeExample2.java
@@ -5,7 +5,7 @@ static void checkPrime(int n){
   int i,m=0,flag=0;      
   m=n/2;      
   if(n==0||n==1){  
-   System.out.println(n+" is not prime number");      
+   System.out.println(n+" is not prime number found");      
   }else{  
    for(i=2;i<=m;i++){      
     if(n%i==0){      
